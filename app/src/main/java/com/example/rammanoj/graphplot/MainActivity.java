a3d51ad605acff278202c7033de075f29875c57b
rammanoj <rammanojpotla1608@gmail.com>
2019-03-12 16:07:07 +0530

    Updated to Pie HCart

--- app/src/main/java/com/example/rammanoj/graphplot/MainActivity.java
+++ app/src/main/java/com/example/rammanoj/graphplot/MainActivity.java
@@ -5,18 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     BarChart barChart;
+    PieChart pieChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,71 +31,32 @@ public class MainActivity extends AppCompatActivity {
     */
     public void setInputs(final String[] labels, float[] ranges)
     {
-        barChart = (BarChart) findViewById(R.id.bargraph);
-        barChart.setDrawBarShadow(false);
-        barChart.setDrawValueAboveBar(true);
-        barChart.setMaxVisibleValueCount(1000);
-        barChart.setPinchZoom(false);
-        barChart.setDrawGridBackground(true);
-        barChart.setTouchEnabled(true);
-        barChart.setDragEnabled(true);
-        barChart.setScaleEnabled(true);
 
+        pieChart = (PieChart) findViewById(R.id.piechart);
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(true);
+        pieChart.setExtraOffsets(5, 10, 5, 5);
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
 
-        // Adding the ranges of the Bar graphs here
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yvalues = new ArrayList<>();
         for(int i=0; i<labels.length; i++) {
-            float f = (float)(i+1);
-            barEntries.add(new BarEntry(f, ranges[i]));
+            yvalues.add(new PieEntry(ranges[i], labels[i]));
         }
-//        int[] colors = new int[ranges.length];
-//        for(int i=0; i<ranges.length; i++) {
-//            if(ranges[i] >= 80) {
-//                // Bar color: Green
-//                colors[i] = Color.GREEN;
-//            } else if(ranges[i] <= 30) {
-//                // Bar color: Red
-//                colors[i] = Color.RED;
-//            } else if(ranges[i] >=60 && ranges[i] < 80) {
-//                // Bar color: Light Green
-//                colors[i] = R.color.LightGreen;
-//            } else if(ranges[i] >=40 && ranges[i] < 60) {
-//                // Bar color: Yellow
-//                colors[i] = Color.YELLOW;
-//            } else if(ranges[i] > 30 && ranges[i] < 40 ) {
-//                // Bar color: Light read
-//                colors[i] = R.color.LightRed;
-//            }
-//        }
 
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Topics");
-//        barDataSet.setColors(colors);
-        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        IAxisValueFormatter formatter = new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                try {
-                    return labels[(int)(value-1)];
-                } catch (Exception e) {
-                    return "";
-                }
-            }
-        };
+        PieDataSet dataSet = new PieDataSet(yvalues, "Topics");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
 
-        BarData barData = new BarData(barDataSet);
-        barData.setBarWidth(0.9f);
-        barChart.setData(barData);
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.WHITE);
 
 
-        // Configuring the X-axis here
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
-        xAxis.setDrawAxisLine(true);
-        xAxis.setDrawGridLines(false);
-        xAxis.setLabelCount(3);
-        xAxis.setGranularityEnabled(true);
-        xAxis.setCenterAxisLabels(false);
-        xAxis.setAxisMinimum(1);
-        xAxis.setValueFormatter(formatter);
+        pieChart.setData(data);
     }
 }

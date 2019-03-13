85991b2053a37a20d1cffba014b304d8178888cc
rammanoj <rammanojpotla1608@gmail.com>
2019-03-13 09:56:17 +0530

    Updated PIE chanet

--- app/src/main/java/com/example/rammanoj/graphplot/MainActivity.java
+++ app/src/main/java/com/example/rammanoj/graphplot/MainActivity.java
@@ -14,22 +14,22 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    BarChart barChart;
     PieChart pieChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] labels = {"permutations", "combinations", "algebra", "probability", "aptitude", "quants", "helloworld"};
+        String[] labels = {"Score"};
         float[] ranges = {12.34f, 34.56f, 56.56f, 98.43f, 65.21f, 68.43f, 43.43f};
-        setInputs(labels, ranges);
+        String topic = "TOpic 1";
+        setInputs(labels, ranges, topic);
     }
     /*
     * labes: String array (topic names, Note::First Topic name has to be empty string)
     * ranges: Float, the performaces of the students in the order of the above array.
     */
-    public void setInputs(final String[] labels, float[] ranges)
+    public void setInputs(final String[] labels, float[] ranges, String topic)
     {
 
         pieChart = (PieChart) findViewById(R.id.piechart);
@@ -38,8 +38,10 @@ public class MainActivity extends AppCompatActivity {
         pieChart.setExtraOffsets(5, 10, 5, 5);
         pieChart.setDragDecelerationFrictionCoef(0.95f);
 
-        pieChart.setDrawHoleEnabled(false);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setCenterText(topic);
         pieChart.setHoleColor(Color.WHITE);
+        pieChart.setDrawEntryLabels(false);
         pieChart.setTransparentCircleRadius(61f);
 
         ArrayList<PieEntry> yvalues = new ArrayList<>();
